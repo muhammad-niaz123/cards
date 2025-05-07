051811874c1bd7d952014d16a4f6d077cba9dfa5
muhammad-niaz123 <niaz.razaa@gmail.com>
2025-05-07 12:29:51 +0500

    updated

--- src/app/components/Filter.js
+++ src/app/components/Filter.js
@@ -1,35 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Filter = ({ setcategory }) => {
+
+
+const [active, setactive] = useState("all")
+
+
   return (
     <>
       <button
-        className=" bg-black/70      md:p-3 rounded  p-2   drop-shadow-xl/50        cursor-pointer    "
-        onClick={() => setcategory("men")}
+        className={` bg-black/70 ${ active === "men" ? "buttonactive" : "" }   md:p-3 rounded  p-2   drop-shadow-xl/50  
+              cursor-pointer    `}
+        onClick={() =>{ setcategory("men")
+          setactive("men")
+        }}
+       
       >
         {" "}
         MENS{" "}
       </button>
 
       <button
-        className="  bg-black/70 md:p-3 rounded p-2   drop-shadow-xl/50    cursor-pointer     "
-        onClick={() => setcategory("women")}
+        className={` bg-black/70 ${ active === "women" ? "buttonactive" : "" }   md:p-3 rounded  p-2   drop-shadow-xl/50  
+        cursor-pointer    `}
+        onClick={() =>{ setcategory("women")
+          setactive("women")
+        }}
       >
         {" "}
         WOMENS{" "}
       </button>
 
       <button
-        className="bg-black/70 md:p-3 rounded p-2   drop-shadow-xl/50      cursor-pointer      "
-        onClick={() => setcategory("kids")}
+        className={` bg-black/70 ${ active === "kids" ? "buttonactive" : "" }   md:p-3 rounded  p-2   drop-shadow-xl/50  
+        cursor-pointer    `}
+        onClick={() =>{ setcategory("kids")
+          setactive("kids")
+        }}
       >
         {" "}
         KIDS{" "}
       </button>
 
       <button
-        className="bg-black/70 md:p-3 rounded p-2  drop-shadow-xl/50          cursor-pointer      "
-        onClick={() => setcategory("all")}
+        className={` bg-black/70 ${ active === "all" ? "buttonactive" : "" }   md:p-3 rounded  p-2   drop-shadow-xl/50  
+        cursor-pointer    `}
+        onClick={() =>{ setcategory("all")
+          setactive("all")
+        }}
       >
         {" "}
         ALL{" "}
